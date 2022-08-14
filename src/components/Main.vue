07460f773f16ab17bebf26f6bc8e5fca4ad7ad02
apple0032 <waihongip@gmail.com>
2022-08-14 08:59:46 +0800

    Update UI

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,22 +6,22 @@
 
         <div v-for="(tabData, index) in this.tabData" :key="index">
           <b-tab :title=tabData.title @click="changeTab(tabData.tab)">
-
             <b-container class="ig_container">
               <b-row>
-                <b-col cols="12" v-for="(data, index2) in ig_data" :key="index2" class="ig_box" @click="clickBox()">
-                  <div v-if="!(data['_source']['media']).includes('mp4')">
-                    <img v-bind:src="data['_source']['media']" />
+                <b-col cols="12" v-for="(data, index2) in ig_data" :key="index2" class="ig_box" @click="clickBox(data['_source']['permalink'])">
+                  <div v-if="!(data['_source']['media_type']).includes('VIDEO')">
+                    <img v-bind:src="data['_source']['media_url']" />
                   </div>
                   <div v-else>
                     <video width="50%" controls>
-                      <source v-bind:src="data['_source']['media']" type="video/mp4">
+                      <source v-bind:src="data['_source']['media_url']" type="video/mp4">
                       Your browser does not support the video tag.
                     </video>
                   </div>
                   <div class="ig_details">
-                    💕 {{data['_source']['like']}}
-                    📅 {{data['_source']['date']}}
+                    💕 {{data['_source']['like_count']}}
+                    🗣️ {{data['_source']['comments_count']}}
+                    📅 {{ showDate( data['_source']['timestamp']) }}
                   </div>
                 </b-col>
               </b-row>
@@ -61,23 +61,31 @@ export default {
           title : 'By Recency',
           tab : 'fresh'
         }
-      ]
+      ],
+      scrolled : false
     }
   },
   methods: {
     async toBottom () {
       //console.log("Triggered")
-      if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight + 20) {
+      //console.log(document.body.scrollHeight);
+      //console.log(window.innerHeight + window.scrollY)
+
+      if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 150) {
 
         //console.log("Bottom");
-        //console.log(this.page);
 
-        var newPage = this.page + 1;
-        //console.log(newPage);
+        if(this.scrolled === false) {
+          this.scrolled = true;
+          var newPage = this.page + 1;
 
-        let ig_data = await this.loadData(this.type,newPage);
-        this.ig_data = this.ig_data.concat( ig_data );
-        this.page = newPage;
+          let ig_data = await this.loadData(this.type, newPage);
+          this.ig_data = this.ig_data.concat(ig_data);
+          this.page = newPage;
+
+          this.scrolled = false;
+        }
+
       }
     },
     async loadData(type,page){
@@ -90,11 +98,8 @@ export default {
                 console.log(error);
               });
 
+      //console.log(ig_data);
       return ig_data;
-      //console.log(this.ig_data);
-    },
-    async clickBox(){
-      window.open('https://www.instagram.com/9gag/', '_blank');
     },
 
     async changeTab(clickType){
@@ -110,51 +115,22 @@ export default {
         let ig_data = await this.loadData(clickType,1);
         this.ig_data = ig_data;
       }
+    },
+    clickBox(permalink){
+      window.open(permalink, '_blank');
+    },
+    showDate(date){
+      date = new Date( date ) ;
+      return date.getFullYear() + "/" +
+              ("00" + (date.getMonth() + 1)).slice(-2) + "/" +
+              ("00" + date.getDate()).slice(-2);
     }
   }
 }
 
-
 </script>
 
 
-
 <style scoped>
-.ig_details{
-  border : 1px solid gray;
-  border-radius: 20px;
-}
-
-.ig_container img{
-  width: 50%;
-}
-
-.ig_box{
-  border : 1px solid #616161;
-  border-radius: 10px;
-  margin-bottom: 10px;
-  padding : 10px;
-  cursor: pointer;
-}
-
-.ig_box:hover{
-  background: rgba(77, 226, 161, 0.43);
-  transition: 0.2s all;
-}
-
-
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  @import '../assets/main.css';
 </style>
